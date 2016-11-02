3f9a188e9d072a18ada2257a7518fd2d938022d2
dongsj <232171773@qq.com>
2016-11-02 18:16:05 +0800

    20161103 dongsj finished

--- 20161103/resolve/dongsj.js
+++ 20161103/resolve/dongsj.js
@@ -1,3 +1,22 @@
 /**
  * Created by dongsj on 2016/11/1.
  */
+function sortNumbers(x,y,z){
+    if(x>y){
+        x=x+y;
+        y=x-y;
+        x=x-y;
+    }
+    if(y>z){
+        y=y+z;
+        z=y-z;
+        y=y-z;
+    }
+    if(x>y){
+        x=x+y;
+        y=x-y;
+        x=x-y;
+    }
+    return [x,y,z];
+}
+console.log(`${sortNumbers(7,5,3)}`);
